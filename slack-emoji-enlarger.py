6ad9b0e880f56f325de2d6a4f5479a98658b9758
Md Abedin <mdabedincs@gmail.com>
2020-08-21 10:34:42 -0400

    add desc

--- slack-emoji-enlarger.py
+++ slack-emoji-enlarger.py
@@ -1,10 +1,11 @@
 import requests
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("filename", help="the image to enlarge and upload")
+parser = argparse.ArgumentParser(description="enlarge an image or gif and split into a grid and upload each grid piece as a slack emoji in order to create the look of an enlarged slack emoji")
+parser.add_argument("filename", help="the image or gif to enlarge and upload")
 args = parser.parse_args()
-print(args.echo)
+print(args.filename)
+    
 quit()
 
 url = 'https://{subdomain}.slack.com/api/emoji.add'
