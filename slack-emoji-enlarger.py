d5a7f57d600688a545e816a8cec612ef9aca4547
Md Abedin <mdabedincs@gmail.com>
2020-08-23 18:22:25 -0400

    add opening of tiles and creation of requests

--- slack-emoji-enlarger.py
+++ slack-emoji-enlarger.py
@@ -22,48 +22,47 @@ parser.add_argument("slack_user_token", help='a slack user token from the slack-
 
 args = parser.parse_args()
 
-"""
-print(args.file_path)
-print(args.size_dimension)
-print(args.size)
-print(args.slack_subdomain)
-print(args.slack_user_token)
-"""
-
-
 Path(args.emoji_base_name).mkdir(exist_ok=True)
 
 SLACK_EMOJI_DIMENSION_SIZE = 128
-resized_path = "{directory}/{file_stem}{file_type}".format(directory=args.emoji_base_name, file_stem=args.emoji_base_name, file_type=Path(args.file_path).suffix)
+
+resized_path = Path("{directory}/{file_stem}{file_type}".format(directory=args.emoji_base_name, file_stem=args.emoji_base_name, file_type=Path(args.file_path).suffix))
 
 resize_cmd = "convert {file_path} -resize {resized_width}x{resized_height} {resized_path}".format(
         file_path=args.file_path,
         resized_width=args.size*SLACK_EMOJI_DIMENSION_SIZE if args.size_dimension == "width" else "",
         resized_height=args.size*SLACK_EMOJI_DIMENSION_SIZE if args.size_dimension == "height" else "",
-        resized_path=resized_path
+        resized_path=str(resized_path)
         ).split()
 
 tile_cmd = "convert {file_path} -crop {slack_emoji_width}x{slack_emoji_height} +repage +adjoin {tile_path}".format(
-        file_path=resized_path,
+        file_path=str(resized_path),
         slack_emoji_width=SLACK_EMOJI_DIMENSION_SIZE,
         slack_emoji_height=SLACK_EMOJI_DIMENSION_SIZE,
-        tile_path=resized_path
+        tile_path=str(resized_path)
         ).split()
 
 print(" ".join(resize_cmd))
 subprocess.run(resize_cmd)
 print(" ".join(tile_cmd))
 subprocess.run(tile_cmd)
-quit()
 
-with open(args.file_path, "rb") as image_file:
-    url = "https://{subdomain}.slack.com/api/emoji.add".format(subdomain=args.slack_subdomain)
-    data = {
-      "mode": "data",
-      "name": args.emoji_base_name,
-      "token": args.slack_user_token
-    }
-    files = {"image": image_file}
+tile_number = 0
+
+while Path("{path_except_suffix}-{tile_number}{file_type}".format(path_except_suffix=resized_path.with_suffix(""), tile_number=tile_number, file_type=resized_path.suffix)).exists():
+    tile_path = Path("{path_except_suffix}-{tile_number}{file_type}".format(path_except_suffix=resized_path.with_suffix(""), tile_number=tile_number, file_type=resized_path.suffix))
+    print(tile_path)
     
-    res = requests.post(url, data=data, files=files, allow_redirects=False)
-    print(res.text)
+    with open(str(tile_path), "rb") as image_file:
+        url = "https://{subdomain}.slack.com/api/emoji.add".format(subdomain=args.slack_subdomain)
+        data = {
+          "mode": "data",
+          "name": "{emoji_base_name}-{tile_number}".format(emoji_base_name=args.emoji_base_name, tile_number=tile_number),
+          "token": args.slack_user_token
+        }
+        files = {"image": image_file}
+
+        print(data)
+        #  req = requests.post(url, data=data, files=files, allow_redirects=False)
+
+    tile_number += 1
